273e6dfa42c22d8d68ef0479f4110246c48e0c45
Woojung0618 <woojung0618@gmail.com>
2022-08-04 21:34:05 +0900

    #3 Detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,12 +1,37 @@
-import React from "react";
+import { connect } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { actionCreators } from "../store";
 
-function Detail() {
+const Detail = ({ toDos, onBtnClick }) => {
+    const id = useParams().id;
+    const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
+    const navigate = useNavigate();
+
+    const handleDel = (id) => {
+        console.log('id', id);
+        onBtnClick(id);
+        navigate("/");
+    };
 
     return (
         <>
-
+            {toDo?.text}
+            Created at: {toDo?.id}
+            <button onClick={() => handleDel(id)}>DEL</button>
         </>
-    )
+    );
+};
+
+function mapStateToProps(state) {
+    return {
+        toDos: state,
+    };
 }
 
-export default Detail;
+function mapDispatchToProps(dispatch, ownProps) {
+    return {
+        onBtnClick: (id) => dispatch(actionCreators.deleteToDo(parseInt(id))),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
--- src/routes/ToDo.js
+++ src/routes/ToDo.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-
-function ToDo({ text, onBtnClick }) {
-    return <li>
+function ToDo({ text, onBtnClick, id }) {
+    return (
+    <li>
+        <Link to={`/${id}`}>
         {text} <button onClick={onBtnClick}>DEL</button>
+        </Link>
     </li>
+    )
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
