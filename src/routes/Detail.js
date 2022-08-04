a3d8fe8e97670b49d71da94919f00de0b1d83dab
Woojung0618 <woojung0618@gmail.com>
2022-08-04 21:05:23 +0900

    #3 REACT REDUX - connect store, mapStateToProps, mapDispatchToProps

---
+++ src/routes/Detail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Detail() {
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Detail;
---
+++ src/routes/Home.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+
+function Home({ toDos, addToDo }) {
+
+    const [text, setText] = useState("");
+
+    function onChange(e) {
+        setText(e.target.value);
+    }
+    function onSubmit(e) {
+        e.preventDefault();
+        addToDo(text)
+        setText("");
+    }
+    return (
+        <>
+        <h1>To Do</h1>
+        <form onSubmit={onSubmit}>
+            <input type="text" value={text} onChange={onChange}/>
+            <button>Add</button>
+        </form>
+        <ul>
+            {JSON.stringify(toDos)}
+        </ul>
+        </>
+    )
+}
+
+function mapStateToProps(state, ownProps) {
+    console.log(state, ownProps);
+    return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch) {
+    console.log(dispatch);
+    return { 
+        addToDo: (text) => dispatch(actionCreators.addToDo(text))
+     };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Home);
